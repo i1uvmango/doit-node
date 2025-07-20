b7ed6ea090b4f70c72294611e430f0a0985c0e40
wjdaksry <xcruisermk@gmail.com>
2025-07-20 13:35:41 +0900

    a

--- myContacts/app.js
+++ myContacts/app.js
@@ -1,7 +1,10 @@
 const express = require("express"); //express 모델 가져오기 // 얘는 외부 모델이라서 "express"이렇게 사용 
 const dbConnect = require('./config/dbConnect'); //config폴더 속 dbConnect라는 모듈 import(require) //얘는 내부 모델이라 경로로 지정
+
 const app = express(); //서버 실행
 
+app.set("view engine","ejs");
+app.set("views", "./views");
 dbConnect();
 
 //body-parser 미들웨어 등록
